f8a78b78df3d35b417c2c3721b3fcb2b7303b948
msarvanm <muthukus@roehampton.ac.uk>
2022-11-30 23:22:15 +0530

    New files uploaded

--- app/app.js
+++ app/app.js
@@ -4,43 +4,70 @@ const express = require("express");
 // Create express app
 var app = express();
 
+
 // Add static files location
 app.use(express.static("static"));
 
+// use pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+app.use(express.urlencoded({extended: true}))
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
-});
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
+app.post("/table-status", function(req, res) {
+    // console.log(req.body)
+    // params = req.body;
+    // var booked_date = params.selected_date;
+    var booked_date = 2022-11-30;
+    if (req.body) {
+        params = req.body;
+        var booked_date = params.selected_date;
+    } else {
+        var booked_date = 2022-11-30;
+    }
+    console.log(booked_date);
+    sql = "select * from booking_details where booked_date=?";
+    db.query(sql, [booked_date]).then(results => {
+        console.log(results)
+        var data = ["0","0","0","0","0","0"];
+        console.log(results.length)
+        if (results) {
+            let i =0;
+            for (rows in results) {
+                
+                data[i] = results[i].table_id;
+                i++;
+            }
+        }
+        res.render('table', {data:data, booked_date})
     });
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
+
+// Homepage for restaurant
+
+app.get("/homepage", function(req, res) {
+    // res.send("Welcome to our restaurant!");
+    res.render('homepage');
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+app.get("/book-table/:table_id", function(req, res) {
+    var table_id = req.params.table_id;
+    res.render('book-table', {table_id});
 });
+
+app.post ('/update-booking', function (req, res) {
+    console.log(req.body)
+    var selected_date = req.body.booked_date;
+    sql = "INSERT INTO booking_details VALUES (?, ?, ?, ?, ?);"
+    db.query(sql, [req.body.id, req.body.booked_date, req.body.name, req.body.email_id, req.body.status]).then(results => {
+    });
+    res.render('homepage');
+});
+
 
 // Start server on port 3000
 app.listen(3000,function(){
